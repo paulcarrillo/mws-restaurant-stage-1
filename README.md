d624a2fa774b72b071ff075a367e2f4490f98c57
Paul Carrillo <paulcarrillo05@gmail.com>
2018-09-22 14:49:14 -0600

    Responsive, Accessibility, and Service Worker project specifications complete

---
+++ README.md
@@ -0,0 +1,42 @@
+# Restaurant Reviews
+
+## Installation
+
+**How to successfully run the application. This application uses Python, make sure it is
+installed on your machine. Must attain an API key from [MapBox](https://www.mapbox.com/install/)**
+
+1. Fork the feedreader repo [GitHub Pages](https://help.github.com/articles/fork-a-repo/).
+2. `cd` into desired folder where you would like the file to live.
+3. Using `git clone` copy the file on to your machine.
+4. Check for Python in the terminal `python --version`.
+5. Install Python if not already installed [Python Docs](https://www.python.org/doc/).
+6. Launch from terminal Python 2: `python -m SimpleHTTPServer 8000`.
+7. Launch from terminal Python 3: `python3 -m http.server 8000`.
+8. Visit the site in your browser at http://localhost:8000.
+
+
+## Why this Project?
+
+>Project is part of the Udacity Front-End Web Developer Program. Project requirements completed
+were a fully responsive site on desktop, tablet, and mobile. Implementation of Web Accessibility
+making content accessible to a wider range of people with disabilities. ServiceWorker and Cache API
+used to provide content when the website is offline.
+
+
+## Resources
+
+* [ServiceWorkers](https://developers.google.com/web/fundamentals/primers/service-workers/).
+* [Flexbox](https://css-tricks.com/snippets/css/a-guide-to-flexbox/).
+* [Grid](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Basic_Concepts_of_Grid_Layout).
+* [MDM](https://developer.mozilla.org/en-US/).
+* [Udacity](https://www.udacity.com/).
+
+
+## Acknowledgments
+The starter code for this project was provided by [Udacity](https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001).
+
+## Author
+
+* [Paul Carrillo](https://www.linkedin.com/in/paul-carrillo/)
+
+## License
---
+++ sw.js
@@ -0,0 +1,126 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/css/responsive.css',
+    '/css/insideresponsive.css',
+    '/data/restaurants.json',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/js/dbhelper.js',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+/*
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});  */
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+
+self.addEventListener('activate', event => {
+	console.log('ServiceWorker Activated');
+	event.waitUntil(
+		caches.keys().then(CACHE_NAMES => {
+			return Promise.all(CACHE_NAMES.map(thisCacheName => {
+				if (thisCacheName !== CACHE_NAME) {
+					console.log('ServiceWorker Removing cached files from ', thisCacheName);
+					return caches.delete(thisCacheName);
+				}
+			}));
+		})
+		.catch(error => {
+			console.log('ServiceWorker Failed to delete ' + thisCacheName + error);
+		})
+	);
+});
+
+
+
+/*
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['pages-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); */
