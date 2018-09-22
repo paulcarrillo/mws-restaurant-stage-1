ab41dfafdf35f96ca1412cc9b1cc852c9858fea3
Paul Carrillo <paulcarrillo05@gmail.com>
2018-09-22 15:03:33 -0600

    Cleaned up HTML

--- js/main.js
+++ js/main.js
@@ -249,14 +249,14 @@ addMarkersToMap = (restaurants = self.restaurants) => {
  * Register Service Worker.
  */
 
- if ('serviceWorker' in navigator) {
-   window.addEventListener('load', function() {
-     navigator.serviceWorker.register('sw.js').then(function(registration) {
-       // Registration was successful
-       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+     // Registration was successful
+     console.log('ServiceWorker registration successful with scope: ', registration.scope);
      }, function(err) {
        // registration failed :(
        console.log('ServiceWorker registration failed: ', err);
      });
    });
- }
+}
--- sw.js
+++ sw.js
@@ -1,35 +1,34 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = "my-site-cache-v1";
 var urlsToCache = [
-    '/',
-    '/index.html',
-    '/restaurant.html',
-    '/css/styles.css',
-    '/css/responsive.css',
-    '/css/insideresponsive.css',
-    '/data/restaurants.json',
-    '/js/main.js',
-    '/js/restaurant_info.js',
-    '/js/dbhelper.js',
-    '/img/1.jpg',
-    '/img/2.jpg',
-    '/img/3.jpg',
-    '/img/4.jpg',
-    '/img/5.jpg',
-    '/img/6.jpg',
-    '/img/7.jpg',
-    '/img/8.jpg',
-    '/img/9.jpg',
-    '/img/10.jpg'
+  "/",
+  "/index.html",
+  "/restaurant.html",
+  "/css/styles.css",
+  "/css/responsive.css",
+  "/css/insideresponsive.css",
+  "/data/restaurants.json",
+  "/js/main.js",
+  "/js/restaurant_info.js",
+  "/js/dbhelper.js",
+  "/img/1.jpg",
+  "/img/2.jpg",
+  "/img/3.jpg",
+  "/img/4.jpg",
+  "/img/5.jpg",
+  "/img/6.jpg",
+  "/img/7.jpg",
+  "/img/8.jpg",
+  "/img/9.jpg",
+  "/img/10.jpg"
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function(event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 /*
@@ -47,65 +46,65 @@ self.addEventListener('fetch', function(event) {
   );
 });  */
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
+    caches.match(event.request).then(function(response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      // IMPORTANT: Clone the request. A request is a stream and
+      // can only be consumed once. Since we are consuming this
+      // once by cache and once by the browser for fetch, we need
+      // to clone the response.
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(function(response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
           return response;
         }
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        var responseToCache = response.clone();
 
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  console.log("ServiceWorker Activated");
+  event.waitUntil(
+    caches
+      .keys()
+      .then(CACHE_NAMES => {
+        return Promise.all(
+          CACHE_NAMES.map(thisCacheName => {
+            if (thisCacheName !== CACHE_NAME) {
+              console.log(
+                "ServiceWorker Removing cached files from ",
+                thisCacheName
+              );
+              return caches.delete(thisCacheName);
             }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
+          })
         );
       })
-    );
+      .catch(error => {
+        console.log("ServiceWorker Failed to delete " + thisCacheName + error);
+      })
+  );
 });
-
-
-self.addEventListener('activate', event => {
-	console.log('ServiceWorker Activated');
-	event.waitUntil(
-		caches.keys().then(CACHE_NAMES => {
-			return Promise.all(CACHE_NAMES.map(thisCacheName => {
-				if (thisCacheName !== CACHE_NAME) {
-					console.log('ServiceWorker Removing cached files from ', thisCacheName);
-					return caches.delete(thisCacheName);
-				}
-			}));
-		})
-		.catch(error => {
-			console.log('ServiceWorker Failed to delete ' + thisCacheName + error);
-		})
-	);
-});
-
-
 
 /*
 self.addEventListener('activate', function(event) {
