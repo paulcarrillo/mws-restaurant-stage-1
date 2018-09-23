332bb739a09c0f7fe90e9de62349a665c5873a0b
Paul Carrillo <paulcarrillo05@gmail.com>
2018-09-22 20:00:58 -0600

    ARIA roles for index and restaurant html updated.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -87,9 +87,9 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   address.innerHTML = restaurant.address;
 
   const image = document.getElementById('restaurant-img');
-  image.className = 'restaurant-img'
+  image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-
+  image.alt = restaurant.name + 'Restaurant';
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 
